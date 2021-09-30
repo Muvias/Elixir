77a0842ee01f76f7f5dae465779f208f17b5ba80
Vinicius Perazza <vi_guepardo@hotmail.com>
2021-09-30 20:38:56 -0300

    elixirFirstContact

---
+++ priv/repo/migrations/20210930204548_create_users_table.exs
@@ -0,0 +1,15 @@
+defmodule Wabanex.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :email, :string
+      add :nome, :string
+      add :password, :string
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:email]) #Para o email não ter a possibilidade de ser repetido
+  end
+end
