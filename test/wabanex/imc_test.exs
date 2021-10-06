5328b3d868a1751d64e6224c8f28cc18b93139d8
Vinicius Perazza <vi_guepardo@hotmail.com>
2021-10-06 18:34:29 -0300

    finishedProject

---
+++ test/wabanex/imc_test.exs
@@ -0,0 +1,26 @@
+defmodule Wabanex.IMCTest do
+  use ExUnit.Case, async: true
+
+  alias Wabanex.IMC
+
+  describe "calculate/1" do
+    test "when the file exists, returns the data" do
+      params = %{"filename" => "students.csv"}
+
+      response = IMC.calculate(params)
+
+      assert "banana" == response
+      # esse teste acima precisa ser revisado, deu erro de argumento!
+    end
+
+    test "when the wrong filename is given, returns an error" do
+      params = %{"filename" => "student.csv"}
+
+      response = IMC.calculate(params)
+
+      expected_response = {:error, "Error while opening the file"}
+
+      assert response == expected_response
+    end
+  end
+end
